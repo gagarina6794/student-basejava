85d6af85534524325921361ad7b401707c7cc875
lera.p2010 <lera.p2010@bk.ru>
2020-09-02 21:47:01 +0300

    write size and read size added in WriteWithException and readWithException; rid of YearMonth duplicate code

--- src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
+++ src/com/urise/webapp/storage/serializer/DataStreamSerializer.java
@@ -30,63 +30,55 @@ public class DataStreamSerializer implements StorageSerialization {
         try (DataInputStream dis = new DataInputStream(is)) {
             String uuid = dis.readUTF();
             String fullName = dis.readUTF();
-            int size = dis.readInt();
             Resume resume = new Resume(uuid, fullName);
-            var contactsList = new EnumMap<ContactType, String>(ContactType.class);
-            readWithException(contactsList.entrySet(),dis, size, ()->{
-                contactsList.put(ContactType.valueOf(dis.readUTF()),dis.readUTF() );
+            readWithException(dis, () -> {
+                resume.getContacts().put(ContactType.valueOf(dis.readUTF()), dis.readUTF());
             });
-            resume.setContacts(contactsList);
-            int sectionSize = dis.readInt();
-            var sectionsList = new EnumMap<SectionType, Section>(SectionType.class);
-            readWithException(sectionsList.entrySet(),dis,sectionSize, ()->{
+            readWithException(dis, () -> {
                 SectionType sectionType = SectionType.valueOf(dis.readUTF());
                 switch (sectionType) {
                     case ACHIEVEMENTS:
                     case QUALIFICATION:
-                        int contentSize = dis.readInt();
                         List<String> contentList = new ArrayList<>();
-                        readWithException(contentList, dis, contentSize, () -> contentList.add(dis.readUTF()));
-                        sectionsList.put(sectionType, new BulletedListSection(contentList));
+                        readWithException(dis, () -> contentList.add(dis.readUTF()));
+                        resume.getSections().put(sectionType, new BulletedListSection(contentList));
                         break;
                     case OBJECTIVE:
                     case PERSONAL:
                         var content = dis.readUTF();
-                        sectionsList.put(sectionType, new SimpleTextSection(content));
+                        resume.getSections().put(sectionType, new SimpleTextSection(content));
                         break;
                     case EDUCATION:
                     case EXPERIENCE:
-                        int contentOrganizationSize = dis.readInt();
                         List<Organization> organizationList = new ArrayList<>();
-                        readWithException(organizationList, dis, contentOrganizationSize, () -> {
+                        readWithException(dis, () -> {
                             String name = dis.readUTF();
                             dis.readUTF();
                             String link = dis.readUTF();
-                            int experienceSize = dis.readInt();
                             List<Organization.Experience> experiences = new ArrayList<>();
-                            readWithException(experiences, dis, experienceSize, () ->
+                            readWithException(dis, () ->
                             {
-                                YearMonth date1 = YearMonth.parse(dis.readUTF(), DateTimeFormatter.ofPattern("uuuu-M"));
-                                YearMonth date2 = YearMonth.parse(dis.readUTF(), DateTimeFormatter.ofPattern("uuuu-M"));
-                                experiences.add(new Organization.Experience(date1, date2, dis.readUTF(), dis.readUTF()));
+                                experiences.add(new Organization.Experience(readYearMonth(dis), readYearMonth(dis), dis.readUTF(), dis.readUTF()));
                             });
                             organizationList.add(new Organization(name, new Link(name, link), experiences));
                         });
-                        sectionsList.put(sectionType, new OrganizationSection(organizationList));
+                        resume.getSections().put(sectionType, new OrganizationSection(organizationList));
                         break;
                 }
             });
-            resume.setSections(sectionsList);
             return resume;
         }
+    }
+
+    private YearMonth readYearMonth(DataInputStream dis) throws IOException {
+        return YearMonth.parse(dis.readUTF(), DateTimeFormatter.ofPattern("uuuu-M"));
     }
 
     private void contactsSerialize(DataOutputStream dos, Resume resume) throws IOException {
         dos.writeUTF(resume.getUuid());
         dos.writeUTF(resume.getFullName());
         Map<ContactType, String> contacts = resume.getContacts();
-        dos.writeInt(contacts.size());
-        writeWithException(contacts.entrySet(), dos, entry->{
+        writeWithException(contacts.entrySet(), dos, entry -> {
             dos.writeUTF(entry.getKey().name());
             dos.writeUTF(entry.getValue());
         });
@@ -94,13 +86,11 @@ public class DataStreamSerializer implements StorageSerialization {
 
     private void sectionsSerialize(DataOutputStream dos, Resume resume) throws IOException {
         Map<SectionType, Section> sections = resume.getSections();
-        dos.writeInt(sections.size());
         writeWithException(sections.entrySet(), dos, entry -> {
             dos.writeUTF(entry.getKey().name());
             switch (entry.getKey()) {
                 case ACHIEVEMENTS:
                 case QUALIFICATION:
-                    dos.writeInt(((BulletedListSection) entry.getValue()).getContent().size());
                     writeWithException(((BulletedListSection) entry.getValue()).getContent(), dos, dos::writeUTF);
                     break;
                 case OBJECTIVE:
@@ -109,12 +99,10 @@ public class DataStreamSerializer implements StorageSerialization {
                     break;
                 case EDUCATION:
                 case EXPERIENCE:
-                    dos.writeInt(((OrganizationSection) entry.getValue()).getContent().size());
                     writeWithException(((OrganizationSection) entry.getValue()).getContent(), dos, item -> {
                         dos.writeUTF(item.getOrganizationName());
                         dos.writeUTF(item.getLink().getName());
                         dos.writeUTF(item.getLink().getUrl());
-                        dos.writeInt(item.getExperiences().size());
                         writeWithException(item.getExperiences(), dos, experience -> {
                             dos.writeUTF(experience.getYearFrom().toString());
                             dos.writeUTF(experience.getYearTo().toString());
@@ -131,12 +119,14 @@ public class DataStreamSerializer implements StorageSerialization {
 
 
     private <T> void writeWithException(Collection<T> collection, DataOutputStream dos, Writable<T> writable) throws IOException {
+        dos.writeInt(collection.size());
         for (var item : collection) {
             writable.writeCollection(item);
         }
     }
 
-    private <T> void readWithException(Collection<T> collection, DataInputStream dis, int size, Readable readable) throws IOException {
+    private void readWithException(DataInputStream dis, Readable readable) throws IOException {
+        int size = dis.readInt();
         for (int i = 0; i < size; i++) {
             readable.readCollection();
         }
