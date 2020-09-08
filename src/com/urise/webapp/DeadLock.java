6d192438f8777821e1ba82533dafb2ad37e1fd7e
lera.p2010 <lera.p2010@bk.ru>
2020-09-08 21:25:44 +0300

    added sleep in DeadLock

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -5,19 +5,28 @@ public class DeadLock {
     public static Object LOCK2 = new Object();
     public static void main(String[] args) throws InterruptedException {
         Thread thread1 = new Thread(() -> {
-           runLock(LOCK1,LOCK2, Thread.currentThread().getName());
+            try {
+                runLock(LOCK1,LOCK2, Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
         Thread thread2 = new Thread(() -> {
-          runLock(LOCK2, LOCK1, Thread.currentThread().getName());
+            try {
+                runLock(LOCK2, LOCK1, Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         });
         thread1.start();
         thread2.start();
     }
 
-    private static void runLock(Object lock1, Object lock2, String threadID){
+    private static void runLock(Object lock1, Object lock2, String threadID) throws InterruptedException {
         synchronized (lock1){
             System.out.println(threadID + " поток держит lock: " + lock1);
             System.out.println(threadID + " поток ждет lock: " + lock2);
+            Thread.sleep(1000);
             synchronized (lock2){
                 System.out.println(threadID + " поток держит lock: " + lock2);
             }
