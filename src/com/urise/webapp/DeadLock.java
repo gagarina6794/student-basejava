aef7fa5d8563ed1c1fab3db5c12d3bc36c087da0
lera.p2010 <lera.p2010@bk.ru>
2020-09-09 20:37:33 +0300

    rid of code duplicate in DeadLock

--- src/com/urise/webapp/DeadLock.java
+++ src/com/urise/webapp/DeadLock.java
@@ -3,31 +3,24 @@ package com.urise.webapp;
 public class DeadLock {
     public static Object LOCK1 = new Object();
     public static Object LOCK2 = new Object();
+
     public static void main(String[] args) throws InterruptedException {
-        Thread thread1 = new Thread(() -> {
-            try {
-                runLock(LOCK1,LOCK2, Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-            try {
-                runLock(LOCK2, LOCK1, Thread.currentThread().getName());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
+        Thread thread1 = new Thread(() -> runLock(LOCK1, LOCK2, Thread.currentThread().getName()));
+        Thread thread2 = new Thread(() -> runLock(LOCK2, LOCK1, Thread.currentThread().getName()));
         thread1.start();
         thread2.start();
     }
 
-    private static void runLock(Object lock1, Object lock2, String threadID) throws InterruptedException {
-        synchronized (lock1){
+    private static void runLock(Object lock1, Object lock2, String threadID) {
+        synchronized (lock1) {
             System.out.println(threadID + " поток держит lock: " + lock1);
             System.out.println(threadID + " поток ждет lock: " + lock2);
-            Thread.sleep(1000);
-            synchronized (lock2){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (lock2) {
                 System.out.println(threadID + " поток держит lock: " + lock2);
             }
         }
